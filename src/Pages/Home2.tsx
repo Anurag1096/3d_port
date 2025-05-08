58b3a415a250bc01e0eb1f360dcda0d912a94ba3
Anurag1096 <anuragqwerty09@outlook.com>
2025-05-08 21:44:48 +0530

    wip: hero section div creation

--- src/Pages/Home2.tsx
+++ src/Pages/Home2.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Home2() {
   return (
-    <section  className='w-screen h-screen '>
-
-      <h4>Anurag</h4>
-    
-    
-    </section>
+    <section className="w-screen h-screen relative bg-white overflow-hidden">
+    <div className="custom-div w-11/12 sm:w-4/5 md:w-3/4 lg:max-w-screen-lg h-96 mx-auto mt-28  rounded-tl-[3.2rem] rounded-br-[3.2rem] bg-[#ebe8e8] bg-[url(Mask-group.svg)] p-4">
+      <h1 className="text-xl sm:text-2xl md:text-3xl font-semibold text-center">Hi</h1>
+     
+    </div>
+  </section>
   )
 }
 
