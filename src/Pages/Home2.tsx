98560542538ed425f1527e2d8148266485eee8ee
Anurag1096 <anuragqwerty09@outlook.com>
2025-05-10 21:20:47 +0530

    wip: Adding files

--- src/Pages/Home2.tsx
+++ src/Pages/Home2.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import SVGComponent from "../compoents/svgComp";
+import SVGComponentMask from "../compoents/svgmask";
+import SVGGroup from "../compoents/SVGGroup";
 function Home2() {
   return (
-    <section className="w-screen h-screen relative bg-white overflow-hidden">
-    <div className="custom-div w-11/12 sm:w-4/5 md:w-3/4 lg:max-w-screen-lg h-96 mx-auto mt-28  rounded-tl-[3.2rem] rounded-br-[3.2rem] bg-[#ebe8e8] bg-[url(Mask-group.svg)] p-4">
-      <h1 className="text-xl sm:text-2xl md:text-3xl font-semibold text-center">Hi</h1>
-     
-    </div>
-  </section>
-  )
+    <section className="w-screen h-screen relative overflow-hidden z-0">
+      <div>
+        <SVGComponent className="w-11/12 sm:w-4/5 md:w-3/4 lg:max-w-screen-xl h-96 mx-auto mt-36 relative z-0" />
+        <SVGComponentMask className="w-11/12 sm:w-4/5 md:w-3/4 lg:max-w-screen-xl h-96 mx-auto mt-36 absolute inset-0 z-10" />
+      
+      <SVGGroup className="hidden sm:inline-block w-4/5 md:w-3/4 lg:max-w-screen-xl h-9 absolute    "/>
+      </div>
+      {/* <div className=" w-11/12 sm:w-4/5 md:w-3/4 lg:max-w-screen-xl  relative">
+
+      </div> */}
+    </section>
+  );
 }
 
-export default Home2
+export default Home2;
---
+++ src/compoents/SVGGroup.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+const SVGGroup = (props) => (
+  <svg
+    width={165}
+    height={53}
+    viewBox="0 0 165 53"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <rect
+      x={14.2891}
+      y={0.0380859}
+      width={10}
+      height={52}
+      rx={5}
+      transform="rotate(15 14.2891 0.0380859)"
+      fill="white"
+    />
+    <rect
+      x={126.902}
+      y={0.0380859}
+      width={10}
+      height={52}
+      rx={5}
+      transform="rotate(15 126.902 0.0380859)"
+      fill="white"
+    />
+    <rect
+      x={70.5955}
+      y={0.0380859}
+      width={10}
+      height={52}
+      rx={5}
+      transform="rotate(15 70.5955 0.0380859)"
+      fill="white"
+    />
+    <rect
+      x={42.4423}
+      y={0.0380859}
+      width={10}
+      height={52}
+      rx={5}
+      transform="rotate(15 42.4423 0.0380859)"
+      fill="white"
+    />
+    <rect
+      x={155.055}
+      y={0.0380859}
+      width={10}
+      height={52}
+      rx={5}
+      transform="rotate(15 155.055 0.0380859)"
+      fill="white"
+    />
+    <rect
+      x={98.7487}
+      y={0.0380859}
+      width={10}
+      height={52}
+      rx={5}
+      transform="rotate(15 98.7487 0.0380859)"
+      fill="white"
+    />
+  </svg>
+);
+export default SVGGroup;
