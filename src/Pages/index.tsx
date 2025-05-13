e5966c2a3994207c314d52cb97b11420f00badff
Anurag1096 <anuragqwerty09@outlook.com>
2025-05-13 20:22:18 +0530

    wip:removing models and adding motion css

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,6 +1,6 @@
-import Home from './Home';
+
 import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
 import Home2 from './Home2';
-export {Home,About, Projects,Home2 , Contact}
+export {About, Projects,Home2 , Contact}
