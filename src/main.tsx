4678ed8bbf2a882b05d6d87a50ba4afe6a8d5f94
Anurag1096 <anuragqwerty09@outlook.com>
2026-01-26 19:38:44 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n/i18n.ts'
+import { registerSW } from "virtual:pwa-register";
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
+  
 )
+registerSW({
+  immediate: true,
+});
